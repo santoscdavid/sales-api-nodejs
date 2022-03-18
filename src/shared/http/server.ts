77cf19aa44cc8442f2c96607cd1a0be30a46a819
DSantos69 <tecnico.davidcavlcante@gmail,com>
2022-03-18 20:22:26 -0300

    #5 added typeorm initial configuration (in progress)

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
+import 'reflect-metadata';
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
-
+import '@shared/typeorm';
 const app = express();
 
 app.use(cors());
