e7a002e323d9816a5c2494eb49b4a0eeeb8576d4
DSantos69 <tecnico.davidcavlcante@gmail,com>
2022-03-24 07:37:20 -0300

    #10 fixed migration class name

--- src/shared/typeorm/migrations/1647799186728-CreateProducts.ts
+++ src/shared/typeorm/migrations/1647799186728-CreateProducts.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateProducts1607437608841 implements MigrationInterface {
+export class CreateProducts1647799186728 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
