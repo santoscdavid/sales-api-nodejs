fac1dced1d40b86b1400cf14dbf5950d9897acf9
DSantos69 <tecnico.davidcavlcante@gmail,com>
2022-04-08 08:37:08 -0300

    #9 added section creation service and authorization middleware (in progress)

---
+++ src/modules/users/middlewares/isAuthenticated.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import AppError from '@shared/errors/AppError';
+import authConfig from '@config/auth';
+
+export default function (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT Token is missing.');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decodeToken = verify(token, authConfig.jwt.secret);
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT Token.');
+  }
+}
--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -1,5 +1,7 @@
 import AppError from '@shared/errors/AppError';
-import { compare, hash } from 'bcryptjs';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -9,12 +11,13 @@ interface IRequest {
   password: string;
 }
 
-// interface IResponse {
-//   user: User;
-// }
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 class CreateSessionService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
     if (!user) {
@@ -27,7 +30,15 @@ class CreateSessionService {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    return user;
+    const token = sign({}, authConfig.jwt.secret, {
+      subject: user.id,
+      expiresIn: authConfig.jwt.expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
