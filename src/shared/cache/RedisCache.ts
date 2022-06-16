0c9e2aebdfa4615a24bfaf9d11befd59517c04ae
DSantos69 <david.csantos69@gmail.com>
2022-06-16 19:45:52 -0300

    #40 finished the RedisCache class and configuring the product list

--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -8,11 +8,22 @@ export default class RedisCache {
     this, (this.client = new Redis(cacheConfig.config.redis));
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public async save(key: string, value: any): Promise<void> {
-    console.log(key, value);
+    await this.client.set(key, JSON.stringify(value));
   }
 
-  // public async recover<T>(key: string): Promise<T | null> {}
+  public async recover<T>(key: string): Promise<T | null> {
+    const data = await this.client.get(key);
+
+    if (!data) {
+      return null;
+    }
+
+    const parsedData = JSON.parse(data) as T;
+
+    return parsedData;
+  }
 
   // public async invalidate(key: string): Promise<void> {}
 }
