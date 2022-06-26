ea9c0ca23f9e0c53160a2856f51177e2fab2764b
DSantos69 <david.csantos69@gmail.com>
2022-06-26 16:08:47 -0300

    #43 added middleware rate limiter

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
@@ -9,34 +9,35 @@ import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 import uploadConfig from '@config/upload';
+import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+
+app.use(rateLimiter);
 
 app.use(pagination);
 
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.use(errors());
 
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      });
-    }
-    console.log(error);
-
-    return response.status(500).json({
+app.use((error: Error, req: Request, res: Response) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
       status: 'error',
-      message: 'internal server error',
+      message: error.message,
     });
-  },
-);
+  }
+  console.log(error);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333! 😎');
