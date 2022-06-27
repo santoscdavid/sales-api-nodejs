2b454dc672c265ab595f48bbc01383372ee5c3f1
DSantos69 <david.csantos69@gmail.com>
2022-06-27 18:15:45 -0300

    #46 infrastructure layer introduced

---
+++ src/modules/customers/infra/http/controllers/CustomersController.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from 'express';
+import CreateCustomerService from '../../../services/CreateCustomerService';
+import DeleteCustomerService from '../../../services/DeleteCustomerService';
+import ListCustomerService from '../../../services/ListCustomerService';
+import ShowCustomerService from '../../../services/ShowCustomerService';
+import UpdateCustomerService from '../../../services/UpdateCustomerService';
+
+export default class CustomersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listCustomers = new ListCustomerService();
+
+    const customers = await listCustomers.execute();
+
+    return response.json(customers);
+  }
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showCustomers = new ShowCustomerService();
+    const customer = await showCustomers.execute({ id });
+
+    return response.json(customer);
+  }
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body;
+
+    const createProduct = new CreateCustomerService();
+
+    const customer = await createProduct.execute({
+      name,
+      email,
+    });
+
+    return response.json(customer);
+  }
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const updateCustomer = new UpdateCustomerService();
+    const product = await updateCustomer.execute({ id, name, email });
+
+    return response.json(product);
+  }
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteCustomer = new DeleteCustomerService();
+
+    await deleteCustomer.execute({ id });
+
+    return response.status(204).json();
+  }
+}
--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,6 +1,6 @@
 import { getCustomRepository } from 'typeorm';
-import Product from '../typeorm/entities/Product';
-import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import Product from '../infra/typeorm/entities/Product';
+import { ProductRepository } from '../infra/typeorm/repositories/ProductsRepository';
 import RedisCache from '@shared/cache/RedisCache';
 
 // interface IPaginateProduct {
